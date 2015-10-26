4f3a92b4acc2ddb7518d7d63ece82d723670716e
Travis CI <brendan.dahl@gmail.com>
2015-10-26 22:43:10 +0000

    Updates

--- offline-worker.js
+++ offline-worker.js
@@ -27,7 +27,7 @@
 
 
 /* eslint-disable quotes, comma-spacing */
-var PrecacheConfig = [["bundle.css","5dc4bc8b4478439995768994d25477dd"],["bundle.js","7d576b15c8b376a2bb80ad1cccf60a2a"],["index.html","31d5c8a7e80e0103358e277663d4a4b6"]];
+var PrecacheConfig = [["bundle.css","5dc4bc8b4478439995768994d25477dd"],["bundle.js","7d576b15c8b376a2bb80ad1cccf60a2a"],["index.html","2f8b978809eb52e06fc66156477c906e"]];
 /* eslint-enable quotes, comma-spacing */
 var CacheNamePrefix = 'sw-precache-v1-mozilla/platatus-' + (self.registration ? self.registration.scope : '') + '-';
 
